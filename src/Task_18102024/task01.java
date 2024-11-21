b1705ebd404e22c84bb5cb015c83f9cc1564b4fe
Aniket <aniketj0106@gmail.com>
2024-11-21 23:42:24 +0530

    All data 11 types

--- src/Task_18102024/task01.java
+++ src/Task_18102024/task01.java
@@ -1,9 +1,44 @@
 package Task_18102024;
 
+import java.util.Scanner;
+
 public class task01 {
+    public static void main(String[] args) {
     /*
-    Create a simple calculator that performs addition,substraction,multiplication, and division,Modus based on user input using switch statement
+    Create a simple calculator that performs addition,subtraction,multiplication, and division,Modulus based on user input using switch statement
     input:- num1,num2,+
     out put:- num1+num2-> print information
      */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int num1 = sc.nextInt();
+        System.out.println("Enter number 2");
+        int num2 = sc.nextInt();
+        System.out.println("Enter any one operator- +,-,*,/,%");
+        char operators = sc.nextLine().charAt(0);
+        double o = 0;
+        switch (operators){
+            case '+':
+                o= num1+num2;
+            break;
+            case '-':
+                o= num1-num2;
+                break;
+                case '*' :
+                o = num1+num2;
+                break;
+            case '/' :
+                o = num1/num2;
+                break;
+            case '%' :
+                o = num1%num2;
+                break;
+            default :
+                System.out.printf("Please enter valid operators");
+                break;
+        }
+        System.out.println("The Final Result");
+        System.out.println();
+        System.out.println(num1+""+operators+""+num2+"="+o);
+    }
 }
--- src/Task_18102024/task02.java
+++ src/Task_18102024/task02.java
@@ -1,11 +1,57 @@
 package Task_18102024;
 
+import java.util.Scanner;
+
 public class task02 {
     public static void main(String[] args) {
         /*
         write a program that takes the integer input(1-12) and print the name of the corresponding month using a switch statement
-        intput --> 12
+        input --> 12
         output --> Dec
          */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter month number between 1-12");
+        int month = sc.nextInt();
+        switch (month){
+            case 1 :
+                System.out.println("Jan");
+                break;
+            case 2 :
+                System.out.println("Feb");
+                break;
+            case 3 :
+                System.out.println("Mar");
+                break;
+            case 4 :
+                System.out.println("Apr");
+                break;
+            case 5 :
+                System.out.println("May");
+                break;
+            case 6 :
+                System.out.println("Jun");
+                break;
+            case 7 :
+                System.out.println("Jul");
+                break;
+            case 8 :
+                System.out.println("Aug");
+                break;
+            case 9 :
+                System.out.println("Sep");
+                break;
+            case 10 :
+                System.out.println("Oct");
+                break;
+            case 11 :
+                System.out.println("Nov");
+                break;
+            case 12 :
+                System.out.println("Dec");
+                break;
+            default:
+                System.out.println("Please enter month number between 1-12");
+                break;
+        }
     }
 }
